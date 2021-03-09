7ed68adb75c6a4206838d123a9ededfe21e9a578
Alexandr <alexandr@fakemail.com>
2021-03-09 09:12:04 +0500

    add cleanup function to Ticker

--- src/Components/Ticker.js
+++ src/Components/Ticker.js
@@ -28,9 +28,13 @@ function Ticker() {
       localStorage.setItem('test-start', testStart.current.toISO())
     }
 
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(DateTime.local().diff(testStart.current).toFormat('hh:mm:ss'))
     }, 1000)
+
+    return function cleanup() {
+      clearInterval(interval)
+    }
   }, [])
 
   return <Container>Viewing this test: {time}</Container>
